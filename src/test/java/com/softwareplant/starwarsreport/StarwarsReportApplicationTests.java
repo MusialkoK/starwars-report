4b56fc1c82c74deb16d7f3e592071bb5d43151e0
Kamil <k.musialkowski@gmail.com>
2021-03-13 10:39:34 +0100

    Initial commit

---
+++ src/test/java/com/softwareplant/starwarsreport/StarwarsReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareplant.starwarsreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarsReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
