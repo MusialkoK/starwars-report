4ee6923a788372b1ead5985b79e5b49d998b0c17
Kamil <k.musialkowski@gmail.com>
2021-03-16 15:39:30 +0100

    restoring to initials

---
+++ src/test/java/com/softwareplant/starwarsreport/StarwarsReportApplication.java
@@ -0,0 +1,14 @@
+
+package com.softwareplant.starwarsreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarwarsReportApplication.class, args);
+    }
+
+}
