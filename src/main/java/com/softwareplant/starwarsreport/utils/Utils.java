f02a14c582b147a3b85932f656bff39c439806d4
Kamil <k.musialkowski@gmail.com>
2021-03-16 15:25:51 +0100

    mid commit

--- src/main/java/com/softwareplant/starwarsreport/utils/Utils.java
+++ src/main/java/com/softwareplant/starwarsreport/utils/Utils.java
@@ -5,5 +5,6 @@ public class Utils {
     public static Long getIdFromUrl(String url) {
         String[] splicedUrl = url.split("/");
         return Long.valueOf(splicedUrl[splicedUrl.length - 1]);
+
     }
 }
